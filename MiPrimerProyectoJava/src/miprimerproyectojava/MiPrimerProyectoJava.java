376dd3ef2f58bea0ea36b6003cb13aead8e0cc25
Harol <harolseptimo2019@gmail.com>
2025-08-19 12:47:44 -0400

    Add files via upload

---
+++ MiPrimerProyectoJava/src/miprimerproyectojava/MiPrimerProyectoJava.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package miprimerproyectojava;
+
+//Asi se importan funciones
+import java.util.Date;
+import java.util.logging.Logger;
+
+public class MiPrimerProyectoJava {
+
+    public static void main(String[] args) {
+        
+        //Asi se crean variables
+        
+        //int edad;
+        //double estatura = 1.78;
+        //boolean estado = false;
+        
+        //edad = 19;
+        //char digitoVerificador = 'k';
+        
+        //String nombre = "Emanuel";
+        //Date fechaNacimiento;
+
+        Persona personal = new Persona();
+        
+        persona1.asignarNombre(nuevoNombre: "Juan");
+        persona1.asignarEdad(nuevaEdad: 18);
+        persona1.asignarAltura(nuevaAltura: 1.87);
+        
+        System.out.println("La persona "+ nombre+ " tiene "+ edad+ " anios"); //Este es el sistema Print (Vaya jalada)
+        
+        /*
+            Este es un comentario
+            de varias filas
+            Viejas :)
+        */
+        
+        // Este es un comentario de una linea
+        
+    }
+}
